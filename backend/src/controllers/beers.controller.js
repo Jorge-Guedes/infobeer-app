2ddb29f6a295ad9053b8cccbc385af9328f102b0
Jorge-Guedes <jorge.guedes.pena@gmail.com>
2025-09-22 18:20:28 +0100

    chore: remove console.log from backend

--- backend/src/controllers/beers.controller.js
+++ backend/src/controllers/beers.controller.js
@@ -43,7 +43,6 @@ export const getBeersByCategory = async (req, res) => {
 export const getRandom = async (req, res) => {
   try {
     const randomBeer = await getRandomBeer();
-    console.log(randomBeer);
     res.json(randomBeer);
   } catch (err) {
     console.log("ERROR en getRandom:", err);
